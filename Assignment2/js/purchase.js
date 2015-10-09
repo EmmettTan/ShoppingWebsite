e80d2559c7aeab40e631b0d147062b372fcd0339
Emmett Tan <UBCEmmettTan@gmail.com>
2015-10-09 15:58:22 -0700

    initialized product associative array

--- Assignment2/js/purchase.js
+++ Assignment2/js/purchase.js
@@ -1,11 +1,27 @@
 var cart = {};
+var products = [];
+
+function init(){
+	products["Box1"] = "$10";
+	products["Box2"] = "$20";
+	products["Clothes1"] = "$20";
+	products["Clothes2"] = "$30";
+	products["Jeans"] = "$50";
+	products["Keyboard"] = "$20";
+	products["KeyboardCombo"] = "$40";
+	products["Mice"] = "$20";
+	products["PC1"] = "$350";
+	products["PC2"] = "$400";
+	products["PC3"] = "$300";
+	products["Tent"] = "$100";
+}
 
 function addToCart(name) {
-	alert("addToCart: " + name)
+	alert("addToCart: " + products[name]);
 }
 
 function removeFromCart(name){
-	alert("removeFromCart: " + name)
+	alert("removeFromCart: " + name);
 }
 
 
