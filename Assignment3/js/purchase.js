10a140c88857cea666a58526c9636020e989a35c
Emmett Tan <UBCEmmettTan@gmail.com>
2015-10-28 15:39:26 -0700

    florence and the machine is dope. made the cartItem Template, time to kill this assignment

--- Assignment3/js/purchase.js
+++ Assignment3/js/purchase.js
@@ -21,30 +21,55 @@ function init(){
 	products["PC2"] = {};
 	products["PC3"] = {};
 	products["Tent"] = {};
+
 	products["Box1"]["quantity"] = 5;
 	products["Box1"]["price"] = 10;
+	products["Box1"]["src"] = "images/Box1_$10.png";
+
+
 	products["Box2"]["quantity"] = 5;
 	products["Box2"]["price"] = 20;
+	products["Box2"]["src"] = "images/Box2_$20.png";
+
 	products["Clothes1"]["quantity"] = 5;
 	products["Clothes1"]["price"] = 20;
+	products["Clothes1"]["src"] = "images/Clothes1_$20.png";
+
 	products["Clothes2"]["quantity"] = 5;
 	products["Clothes2"]["price"] = 30;
+	products["Clothes2"]["src"] = "images/Clothes2_$30.png";
+
 	products["Jeans"]["quantity"] = 5;
 	products["Jeans"]["price"] = 50;
+	products["Jeans"]["src"] = "images/Jeans_$50.png";
+
 	products["Keyboard"]["quantity"] = 5;
 	products["Keyboard"]["price"] = 20;
+	products["Keyboard"]["src"] = "images/Keyboard_$20.png";
+
 	products["KeyboardCombo"]["quantity"] = 5;
 	products["KeyboardCombo"]["price"] = 40;
+	products["KeyboardCombo"]["src"] = "images/KeyboardCombo_$40.png";
+
 	products["Mice"]["quantity"] = 5;
 	products["Mice"]["price"] = 20;
+	products["Mice"]["src"] = "images/Mice_$20.png";
+
 	products["PC1"]["quantity"] = 5;
 	products["PC1"]["price"] = 350;
+	products["PC1"]["src"] = "images/PC1_$350.png";
+
 	products["PC2"]["quantity"] = 5;
 	products["PC2"]["price"] = 400;
+	products["PC2"]["src"] = "images/PC2_$400.png";
+
 	products["PC3"]["quantity"] = 5;
 	products["PC3"]["price"] = 300;
+	products["PC3"]["src"] = "images/PC3_$300.png";
+
 	products["Tent"]["quantity"] = 5;
 	products["Tent"]["price"] = 100;
+	products["Tent"]["src"] = "images/Tent_$100.png";
 
 	updateTimeRemainingTimer = setTimeout(updateInactiveTime, 1000);
 	timeoutAlertTimer = setTimeout(displayTimeoutAlert, inactiveTime);
@@ -86,7 +111,14 @@ function updateInactiveTime(){
 	updateTimeRemainingTimer = setTimeout(updateInactiveTime, 1000)
 }
 
+function modalAddToCart(caller){
+	var key = getModalItemName(caller);
+	addToCart(key);
+}
 
+function getModalItemName(caller){
+	return caller.parentNode.parentNode.getAttribute("item")
+}
 
 function addToCart(productName) {
 	clearTimeout(timeoutAlertTimer);
@@ -129,6 +161,10 @@ function removeFromCart(productName){
 	updateRemoveBtnVisibility();
 }
 
+function modalRemoveFromCart(caller){
+	var key = getModalItemName(caller);
+	removeFromCart(key);
+}
 
 
 function updateCartPrice(){
@@ -144,9 +180,11 @@ function updateAddBtnVisibility(){
 	for(var key in products){
 		if(!productInStock(key)){
 			document.getElementById(key).classList.add('addBtnHidden');
+			if(document.getElementById("modalAddBtn-" + key)) document.getElementById("modalAddBtn-" + key).classList.add('addBtnHidden'); 
 			// document.querySelector('#' + key + ':hover ' + '.addBtn').style.visibility = 'hidden';
 		}else{
 			document.getElementById(key).classList.remove('addBtnHidden');
+			if(document.getElementById("modalAddBtn-" + key)) document.getElementById("modalAddBtn-" + key).classList.remove('addBtnHidden'); 
 			// document.querySelector('#' + key + ' ' + '.addBtn').style.visibility = 'visible';
 		}
 	}
@@ -156,12 +194,25 @@ function updateRemoveBtnVisibility(){
 	for(var key in products){
 		if(!itemExistsInCart(key)){
 			document.getElementById(key).classList.add('removeBtnHidden');
+			removeElementById("modalItem-" + key);
+			// if(document.getElementById("modalRemoveBtn-" + key)) document.getElementById("modalRemoveBtn-" + key).classList.add('removeBtnHidden'); 
 			// document.querySelector('#' + key + ':hover ' + '.addBtn').style.visibility = 'hidden';
 		}else{
 			document.getElementById(key).classList.remove('removeBtnHidden');
+			if(document.getElementById("modalRemoveBtn-" + key)) document.getElementById("modalRemoveBtn-" + key).classList.remove('removeBtnHidden'); 
 			// document.querySelector('#' + key + ' ' + '.addBtn').style.visibility = 'visible';
 		}
 	}
+}
+
+
+function removeElementById(el){
+	if(!document.getElementById(el)) {
+		console.log("removeElementById(" + el + ") does not exist.");
+		return;
+	}
+	var parent = document.getElementById(el).parentNode;
+	parent.removeChild(document.getElementById(el));
 }
 
 function productInStock(productName){
